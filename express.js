a444024fadf20381e09fd28ed929a9f32cd58ef2
holma198 <holma198@morris.umn.edu>
2019-04-19 16:29:41 -0500

    create login screen

--- express.js
+++ express.js
@@ -8,3 +8,30 @@ var express=require('express'),
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
+var DoQuery = function(sql)
+{
+    return dbf.query(mysql.format(sql));
+}
+
+app.get("/login", function(req, res)
+{
+    var uname = req.param('uname');
+    var pword = req.param('pword');
+
+    var sql = "SELECT * FROM dnd_users WHERE user = '" + uname + "' AND password = '" + pword + "';";
+    var pResult = DoQuery(sql);
+
+    var pResolve = Promise.resolve(pResult);
+    pResolve.then(function(rows)
+    {
+        //If the result is not null we found a valid login
+        if(!rows[0])
+        {
+            res.send(false);
+        }
+        else
+        {
+            res.send(true);
+        }
+    });
+});
---
+++ public/characterCreator.js
@@ -0,0 +1,40 @@
+angular.module('characterCreator', [])
+    .controller('mainCtrl', MainCtrl)
+    .factory('mainApi', mainApi)
+    //.factory('transactionApi', transactionApi)
+    .constant('apiUrl', 'http://localhost:1337'); // CHANGED for the lab 2017!
+
+function MainCtrl($scope, mainApi) {
+    $scope.logOut = logOut;
+    $scope.logged_in = false;
+    $scope.logIn = logIn;
+    $scope.uname = '';
+    $scope.pword = '';
+
+
+    function logIn(uname, pword) {
+        console.log("Trying to log in with name " + uname + " and password " + pword);
+        mainApi.logInUser(uname, pword).success(function (rows)
+        {
+            $scope.logged_in = rows;
+        }).error(function (rows)
+        {
+            console.log(rows);
+        });
+    }
+
+    function logOut() {
+        $scope.logged_in = !$scope.logged_in;
+        console.log($scope.logged_in);
+    }
+
+}
+
+function mainApi($http, apiUrl) {
+    return {
+        logInUser: function (uname, pword) {
+            var url = apiUrl + '/login?uname=' + uname + '&pword=' + pword;
+            return $http.get(url);
+        }
+    };
+}
