c837a7dad503aacf1f3bb43b5fb882bb12d4dd74
UtkarshKr007 <kumar375@morris.umn.edu>
2019-04-26 18:11:09 -0500

    create function createCharacter and variable races for the character "create a character" page

--- public/characterCreator.js
+++ public/characterCreator.js
@@ -11,23 +11,40 @@ function MainCtrl($scope, mainApi)
     $scope.logIn = logIn;
     $scope.uname = '';
     $scope.pword = '';
-
+    $scope.createCharacter = createCharacter;
+    $scope.races = null;
 
     function logIn(uname, pword)
     {
         console.log("Trying to log in with name " + uname + " and password " + pword);
-        mainApi.logInUser(uname, pword).success(function (rows)
+        mainApi.logInUser(uname, pword).success(function (booleanResponse)
         {
-            $scope.logged_in = rows;
-        }).error(function (rows)
+            $scope.logged_in = booleanResponse;
+        }).error(function (booleanResponse)
         {
-            console.log(rows);
+            console.log(booleanResponse);
         });
     }
 
     function logOut()
     {
         $scope.logged_in = !$scope.logged_in;
+    }
+
+    // This can be a generic doFunction(pageNum) function that takes in input,pageNum, from html and then we have a switch(PageNum)
+    // statement after mainApi.changePage(pageNum).success.... to see which value should store the response
+    // or we create a function for each thing, ex: createCharacter and this way I guess we are writing more code, but being more
+    // explicit and clear what a function means
+    function createCharacter()
+    {
+        $scope.pageNum = 1;
+        console.log("User " + $scope.uname + " wants to create a new character by going to pageNum " + $scope.pageNum);
+        mainApi.changePage($scope.pageNum).success(function (raceRows) {
+            $scope.races = raceRows;
+        }).error(function (raceRows)
+        {
+            console.log("Error while retrieving races " + raceRows);
+        });
     }
 
 }
@@ -38,6 +55,12 @@ function mainApi($http, apiUrl)
         logInUser: function (uname, pword)
         {
             var url = apiUrl + '/login?uname=' + uname + '&pword=' + pword;
+            return $http.get(url);
+        },
+        // This is a abstract function that can be called with a value between 1-10 to reach different pages
+        changePage: function (pageNum)
+        {
+            var url = apiUrl + '/meta?page=' + pageNum;
             return $http.get(url);
         }
     };
